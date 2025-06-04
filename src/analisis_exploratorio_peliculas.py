1b3c2b11ba8fe3aa61d81ba6936cf027d91358a7
Hengerberg <alexander.vegas.93@gmail.com>
2025-06-04 08:56:14 +0200

    practica de analisis exploratorio v1

--- src/analisis_exploratorio_peliculas.py
+++ src/analisis_exploratorio_peliculas.py
@@ -5,7 +5,7 @@ import ast
 
 url = 'https://raw.githubusercontent.com/justmarkham/pandas-videos/master/data/imdb_1000.csv'
 df = pd.read_csv(url)
-#print(df.head())
+
 # agregamos una nueva columna de la duracion en minutos
 df["duration_hours"] = pd.to_datetime(df["duration"], unit="m")
 
@@ -17,22 +17,13 @@ amount_genre = df_genre.count()
 
 # ordenamos el df por rating
 df_sort_rating = df.sort_values("star_rating", ascending= False)
+
 # guardamos las mejores 5 peliculas
 top_peliculas = df_sort_rating[0:5]
-
 df_sort_duration = df.sort_values("duration_hours", ascending= False)
-# guardamos las mejores 5 peliculas
+
+# ordenamos la peliculas con mayor duracion
 mayor_duration = df_sort_duration[0:5]
-
-# con el metodo describe analizamos los posibles outliers que pueden haber
-# en este caso observamos que en la columna duration si pueden existir anomalias
-#print(df.describe())
-
-# graficamos las columnas y decidimos analizar con mas profundidad la columna duration
-#df.hist(bins=50)
-#plt.show()
-
-column_duration = df["duration"]
 
 # creamos un diagrama de caja y bigotes y comprobamos los valores atipicos
 plt.figure(figsize=(10,6))
@@ -44,7 +35,6 @@ plt.ylabel("Minutos")
 
 Q3 = df["duration"].quantile(0.75)
 Q1 = df["duration"].quantile(0.25)
-# quant_df  = df.quantile([0.25, 0.75])
 
 # calculamos el rango intercuartilico (IQR)
 iqr = Q3 - Q1
@@ -69,19 +59,18 @@ plt.tight_layout() # ajusta los subplots para evitar superposiciones
 
 # creamos un diagrama de dispersion generado por la siguiente hipotesis:
 # mayor duracion mejor rating
-
 plt.figure(figsize=(10,6))
 plt.subplot()
 sns.scatterplot(x = df_sin_outliers["star_rating"], y = df_sin_outliers["duration"])
-# concluimos que la duracion no tiene una ralacion clara sobre el rating
+plt.title("Diagrama de dispercion")
+plt.xlabel("Rating")
+plt.ylabel("Duracion")
 
-#plt.show()
+plt.show()
 
 # agrupamos en un df los generos y sacamos el promedio de rating por cada genero
 # y ordenamos el df agrapado para mostrar claramente que genero tiene un mejor rating
 df_genre_rating = df.groupby("genre")["star_rating"].mean().sort_values(ascending=False)
-
-# los generos que tienen mayores rating son: Western 8.255556, Film-Noir 8.033333, History 8.000000
 
 df['actors_list'] = df['actors_list'].apply(ast.literal_eval) # convertimos las cademas a objetos tipo lista
 df_exploded = df.explode('actors_list') # creamos un df con una fila por actor
@@ -99,4 +88,21 @@ actor_summary = df_exploded.groupby('actors_list').agg({
 
 # Filtrar actores con al menos 5 películas y ordenar por rating
 top_actors = actor_summary[actor_summary['movie_count'] >= 5].sort_values(by='avg_rating', ascending=False)
-print(top_actors.head(10))
+
+'''
+INSIGHTS
+
+1. ordenando la data por rating de mayor a menor podemos decir con certeza que las peliculas
+que tienen un mayor rating pertenecen a la categoria de crimen.
+
+2. analizando los datos y eliminando los valores atipicos y creando un grafico de caja y bigote del rating y la duracion
+llegamos a la conclusion que no tienen una relacion clara, es decir que una pelicula para tener mayor rating 
+no necesariamente puede ser corta o larga.
+
+3. analizando los generos, podemos definir que los generos que tienen mayores rating son: Western (8.255556), 
+Film-Noir (8.033333), History (8.000000)
+
+4. con respecto a los actores mas productivos podemos decir que los que tienen mayor rating no tienen no son los mas
+productivos en cuanto a cantidad de peliculas, sin embargo los actores que tienen mas cantidad de peliculas 
+tienen mayores probabilidad de tener mayor rating
+'''
